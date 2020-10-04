6876ab0826cebad19665b046f88437afc12770ee
preyansh98 <preyansh98@gmail.com>
2020-10-04 11:15:03 -0400

    update to be cross origin

--- app/app.py
+++ app/app.py
@@ -1,15 +1,20 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 from classifier import ModelWrapper
 import pickle
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 model = pickle.load(open('model.pkl','rb'))
 
 @app.route("/")
+@cross_origin()
 def index():
     return "Hello World"
 
 @app.route("/predict_tweet", methods=['POST'])
+@cross_origin()
 def predict_tweet():
     req_data = request.get_json(force=True)
     print(req_data)
